01cc21fe41158a6af61f6bae4e6655be7e3a5ddd
Ramkrishna <ramkrishna2k3@gmail.com>
2023-06-04 16:56:46 +0530

    updated secret and access key

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAU5OVVT5TENUMEHDZ"
-  secret_key = "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"
+  access_key = "access_key"
+  secret_key = "secret_key"
 }
 
 module "webserver-1" {
