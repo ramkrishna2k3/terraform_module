be5a96bc27909e3effb5a7c222dbb36412ce3167
Ramkrishna <ramkrishna2k3@gmail.com>
2023-06-04 21:13:23 +0530

    modules-updated

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "access_key"
-  secret_key = "secret_key"
+  access_key = "accesskey"
+  secret_key = "secretkey"
 }
 
 module "webserver-1" {
